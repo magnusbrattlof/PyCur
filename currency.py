c2b8e1c12d512771b3cda588ac7ecb68283b6735
Magnus <brattlof.magnus@gmail.com>
2017-10-22 16:00:30 +0200

    Hello world

---
+++ currency.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+# Initalization of variables and other stuff
+BASE_URL = 'https://api.fixer.io/'
+
+class Pycur:
+
+    def __init__(self, base=None):
+        self.base = base
+        
+    def get_currency(self):
+        self.URL = BASE_URL + 'latest?base={}'.format(self.base)
+        self.raw_data = requests.get(self.URL).text
+        self.data = json.loads(self.raw_data)
+        return self.data['rates']
+
+    def get_historical(self, date):
+        self.date = date
+        self.URL = BASE_URL + '{}?base={}'.format(self.date, self.base)
+        self.raw_data = requests.get(self.URL).text
+        self.data = json.loads(self.raw_data)
+        return self.data['rates']
